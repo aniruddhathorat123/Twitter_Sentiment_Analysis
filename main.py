d42a293fe1663b0acc9aea4da92f63aa22e72438
aniruddhathorat123 <aniruddhathorat123@gmail.com>
2018-06-03 22:41:13 +0530

    Version 2.0

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ def percentage (part, whole):
     return 100*float(part)/float(whole)
 
 def call_sentiment():
-    consumerKey = 'tJ0oqPYlQWrbfXGwty619zwdM'
-    consumerSecret = 'v7bue06QFpUPEtRyBdw86YA5qfCdte6hk9DbkHBFVNLJBTGMe0'
-    accessToken = '2958139430-xp4nXrgEzeIRadGW5DiknA5YbQdKAwJ4B9xeHyb'
-    accessTokenSecret = 'hO3L5JZaAiVW5P9rSL9gnaW7z3TggZnDsoPFJYS9iDeLI'
+    
 
     auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
     auth.set_access_token(accessToken, accessTokenSecret)
@@ -29,22 +26,52 @@ def call_sentiment():
     negative = 0
     neutral = 0
     polarity = 0
-    tw = ""
+    tw=""
+    r = 7
+
 
     for tweet in tweets:
-        twets.insert(END, tweet.text+"\n---------\n")
-        #tw = tw+tweet.text+"\n"
+        tweet_text = tweet.text
+        char_list = [tweet_text[j] for j in range(len(tweet_text)) if ord(tweet_text[j]) in range(65536)]
+        tweet_t = ''
+        for j in char_list:
+            tweet_t = tweet_t + j
         analysis = TextBlob(tweet.text)
         polarity += analysis.sentiment.polarity
 
+        twets = ScrolledText(root, width=80, height=5)
+        tid = ScrolledText(root, width=20, height=5)
+        pola = ScrolledText(root, width=20, height=5)
+        result = ScrolledText(root, width=20, height=5)
+
+        tid.grid(row=r)
+        twets.grid(row=r, column=1)
+        pola.grid(row=r, column=2)
+        result.grid(row=r, column=3)
+        r=r+1
+
+
         if (analysis.sentiment.polarity == 0):
             neutral += 1
+            tid.insert(END, tweet.id)
+            twets.insert(END, tweet_t)
+            pola.insert(END, analysis.sentiment.polarity)
+            result.insert(END, "Neutral")
+
 
         elif (analysis.sentiment.polarity > 0.00):
             positive += 1
+            tid.insert(END, tweet.id)
+            twets.insert(END, tweet_t)
+            pola.insert(END, analysis.sentiment.polarity)
+            result.insert(END, "Positive")
 
         elif (analysis.sentiment.polarity < 0.00):
             negative += 1
+            tid.insert(END, tweet.id)
+            twets.insert(END, tweet_t)
+            pola.insert(END, analysis.sentiment.polarity)
+            result.insert(END, "Negative")
 
     #twets.insert(END, tw)
 
@@ -80,22 +107,31 @@ def call_sentiment():
 
 root = Tk()
 root.title("Twitter Sentiment Analysis")
-root.geometry("1000x1000")
+root.geometry("1500x1000")
+
 
 
 st = Entry(root)
 nst = Entry(root)
 search_lbl = Label(root, text = "Keyword:")
 no_twt_lbl = Label(root, text = "number of tweets")
-twets = ScrolledText(root, width = 100, height = 100)
 
 bt1 = Button(root, text = "Make Sentiment", command = call_sentiment)
+lbl_uname = Label(root, text = "Username")
+lbl_tweet = Label(root, text = "Tweet")
+lbl_polarity = Label(root, text = "Polarity")
+lbl_result = Label(root, text = "Result")
 
-search_lbl.grid(row=0)
-st.grid(row=0, column=1)
-no_twt_lbl.grid(row=1, column=0)
-nst.grid(row=1, column=1)
-bt1.grid(row=3, column=1)
-twets.grid(row=4 , columnspan = 3)
+
+search_lbl.grid(row=0,column = 1)
+st.grid(row=0, column=2)
+no_twt_lbl.grid(row=1, column=1)
+nst.grid(row=1, column=2)
+bt1.grid(row=3, column=2)
+lbl_uname.grid(row=5)
+lbl_tweet.grid(row=5,column=1)
+lbl_polarity.grid(row=5,column = 2)
+lbl_result.grid(row=5, column= 3)
+
 
 root.mainloop()
